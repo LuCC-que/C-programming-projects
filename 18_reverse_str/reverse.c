d375d64c05f2b701dd942f752cb371989fac14cd
LuCC-que <chen1968493689@users.github.com>
2022-06-21 20:09:23 -0400

    push to repo

---
+++ 18_reverse_str/reverse.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char * str) {
+  if(!str) return;
+
+  size_t string_len = 0;
+  char temp;
+  const char* ptr = str;
+  while(*ptr != '\0'){
+    ++string_len;
+    ++ptr;
+  }
+
+  for(size_t i = 0; i < (string_len / 2); ++i){
+    temp = str[i];
+    str[i] = str[string_len -1 -i];
+    str[string_len -1 -i] =temp;
+  }
+}
+
+int main(void) {
+  char str0[] = "";
+  char str1[] = "123";
+  char str2[] = "abcd";
+  char str3[] = "Captain's log, Stardate 42523.7";
+  char str4[] = "Hello, my name is Inigo Montoya.";
+  char str5[] = "You can be my wingman anyday!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+  for (int i = 0; i < 7; i++) {
+    reverse(array[i]);
+    printf("%s\n", array[i]);
+  }
+  return EXIT_SUCCESS;
+}
